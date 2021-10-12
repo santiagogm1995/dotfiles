9ddae0ea060767f2c145e393e2dd6107ef2134e7
Santiago González <santiagogm.prog@gmail.com>
2021-10-12 19:34:53 +0200

    Delete ExampleCICDGitlab directory

--- ExampleCICDGitlab/readme.md
+++
@@ -1,6 +0,0 @@
-# TestSapui5
-Insert the purpose of this project and some intersting infos here
-
-
-## Credits
-This project has been generated with 💙 and [easy-ui5](https://github.com/SAP)
--- ExampleCICDGitlab/uimodule/webapp/controller/Main.controller.js
+++
@@ -1,8 +0,0 @@
-sap.ui.define([
-  "com/test/TestSapui5/controller/BaseController"
-], function(Controller) {
-  "use strict";
-
-  return Controller.extend("com.test.TestSapui5.controller.Main", {
-  });
-});
